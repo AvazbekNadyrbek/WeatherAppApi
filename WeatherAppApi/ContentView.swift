8512f597e0e66153511e86d99d891dd2471eb1c2
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-10-08 09:34:34 +0200

    Initial Commit

---
+++ WeatherAppApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppApi
+//
+//  Created by Авазбек Надырбек уулу on 08.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppApi/WeatherAppApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApiApp.swift
+//  WeatherAppApi
+//
+//  Created by Авазбек Надырбек уулу on 08.10.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
